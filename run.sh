fa0bcff5f25f6f0cf866ccf095726574b2504a9d
arnav-makkar <142909930+arnav-makkar@users.noreply.github.com>
2025-04-18 19:10:17 +0530

    deploy fix1

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-pip install -r requirements.txt
-python scripts/download_images.py
-python scripts/extract_features.py
-python scripts/match.py b1.jpg
-python scripts/match.py b2.jpg
+# pip install -r requirements.txt
+# python scripts/download_images.py
+# python scripts/extract_features.py
+# python scripts/match.py b1.jpg
+# python scripts/match.py b2.jpg
 streamlit run app.py
